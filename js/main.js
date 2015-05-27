cda2727ba9a8b594018b4e7184033d4381f95b66
Lucas Ribeiro <lucasribeiro1990@gmail.com>
2015-05-26 22:45:24 -0300

    Reformat jason files and remove unnecessary vars
    Reformat jason files and remove unnecessary vars

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 function setLanguage(lang) {
-	var ptbr = "http://raw.githubusercontent.com/lucasribeiro1990/lucasribeiro1990.github.io/master/json/pt-br.json";
-	var enus = "http://raw.githubusercontent.com/lucasribeiro1990/lucasribeiro1990.github.io/master/json/en-us.json";
 	switch(lang) {
 		case "pt-br":
 			$.getJSON( 'json/pt-br.json', function( data ) {
