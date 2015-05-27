9d46c4b93e39903ae0fad8094bc1b9425cbb3cec
Lucas Ribeiro <lucasribeiro1990@gmail.com>
2015-05-26 22:35:47 -0300

    Adds the initial script to implement language set

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 function setLanguage(lang) {
+	var ptbr = "http://raw.githubusercontent.com/lucasribeiro1990/lucasribeiro1990.github.io/master/json/pt-br.json";
+	var enus = "http://raw.githubusercontent.com/lucasribeiro1990/lucasribeiro1990.github.io/master/json/en-us.json";
 	switch(lang) {
-		case 'pt-BR':
-			
-		case 'en-US':
-			
+		case "pt-br":
+			$.getJSON( 'json/pt-br.json', function( data ) {
+				alert(data.resume);
+				alert(data.exp[0].position);
+			});
+			break;	
+		case "en-us":
+			$.getJSON( 'json/en-us.json', function( data ) {
+				alert(data.resume);
+				alert(data.exp[0].position);
+			});
 		default:
-			
+			$.getJSON( 'json/pt-br.json', function( data ) {
+				alert(data.resume);
+				alert(data.exp[0].position);
+			});
+			break;
 	}
 }
