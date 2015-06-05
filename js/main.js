e799e903bc6e0f66d457948099e93008096d4298
Lucas Ribeiro <lucasribeiro1990@gmail.com>
2015-06-05 12:31:52 -0300

    Modifis the json files to http and adds debug info on main script

--- js/main.js
+++ js/main.js
@@ -1,23 +1,62 @@
-function setLanguage(lang) {
-	var result;
-	switch(lang) {
-		case "pt-br":
-			$.getJSON( 'json/pt-br.json', function( data ) {
-				result = data;
-			});
-			alert(JSON.stringify(result));
-			break;	
-		case "en-us":
-			$.getJSON( 'json/en-us.json', function( data ) {
-				result = data;
-			});
-			alert(JSON.stringify(result));
-			break;
-		default:
-			$.getJSON( 'json/pt-br.json', function( data ) {
-				result = data;
-			});
-			alert(JSON.stringify(result));
-			break;
+function setLanguage(lang){
+	ptBR={
+			resume: "Portuguese Resume",
+			exp: [
+			      {
+			    	  position: "B",
+			    	  company: "C",
+			    	  period: "D",
+			    	  description:"E"
+			      },
+			      {
+			    	  position: "F",
+			    	  company: "G",
+			    	  period: "H",
+			    	  description:"I"
+			      },
+			      {
+			    	  position: "J",
+			    	  company: "K",
+			    	  period: "L",
+			    	  description: "M"
+			      }
+			      ]
+	};
+	enUS={
+			resume: "English Resume.",
+			exp: [
+			      {
+			    	  position: "O",
+			    	  company: "P",
+			    	  period: "Q",
+			    	  description:"R"
+			      },
+			      {
+			    	  position: "S",
+			    	  company: "T",
+			    	  period: "U",
+			    	  description:"V"
+			      },
+			      {
+			    	  position: "X",
+			    	  company: "Z",
+			    	  period: "K",
+			    	  description: "W"
+			      }
+			      ]
+	};
+
+	switch(lang){
+	case "pt-br":
+		$("#resume-title").text("Resumo");
+		$("#resume-text").text(ptBR.resume);
+		break;	
+	case "en-us":
+		$("#resume-title").text("Resume");
+		$("#resume-text").text(enUS.resume);
+		break;
+	default:
+		alert(ptBR);
+		break;
 	}
 }
